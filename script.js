2009a0b2e47ba875c31773005490270ddfcacc29
barbara-musial <basia.musial22@op.pl>
2021-10-11 20:03:11 +0200

    Adding clear function, working on action functions

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function tap(n) {
     document.getElementById("win").innerHTML += n;
 }
-function cl(){
+
+function clr() {
+    document.getElementById("win").innerHTML = ' ';
+    document.getElementById("hist").innerHTML = ' ';
+    document.getElementById("score").innerHTML = ' ';
+}
+
+function a() {
+    var val = document.getElementById("win").innerHTML
+    document.getElementById("hist").innerHTML = val + " + ";
+    document.getElementById("win").innerHTML = ' ';
+}
+
+function s() {
+    document.getElementById("his").innerHTML = document.getElementById("win").innerHTML + " - ";
+    document.getElementById("win").innerHTML = ' ';
+}
+
+function d() {
+    document.getElementById("his").innerHTML = val + " / ";
     document.getElementById("win").innerHTML = ' ';
 }
